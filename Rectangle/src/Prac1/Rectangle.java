fe4214280717acda8eeb57a4b5f73b1e09e272a3
laukik02-tech <laukikr12@gmail.com>
2024-09-23 12:33:09 +0530

    1st commit

---
+++ Rectangle/src/Prac1/Rectangle.java
@@ -0,0 +1,27 @@
+package Prac1;
+
+public class Rectangle 
+{
+	float length;
+	float breadth;
+	float area;
+	float perimeter;
+	public float getLength() {
+		return length;
+	}
+	public void setLength(float length) {
+		this.length = length;
+	}
+	public float getBreadth() {
+		return breadth;
+	}
+	public void setBreadth(float breadth) {
+		this.breadth = breadth;
+	}
+	public float getPerimeter() {
+		return perimeter;
+	}
+	public void setPerimeter(float perimeter) {
+		this.perimeter = perimeter;
+	}
+}
